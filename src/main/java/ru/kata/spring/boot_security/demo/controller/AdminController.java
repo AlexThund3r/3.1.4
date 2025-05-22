4cdeace414d471c9d790a91db312b002fc5095e2
AlexThund3r <Alexander.Belov.offical@yandex.ru>
2025-05-22 22:25:26 +0300

    Final commit

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -4,15 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.Role;
-import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.AdminService;
 import ru.kata.spring.boot_security.demo.service.RoleService;
-import ru.kata.spring.boot_security.demo.service.UserService;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Controller
 @RequestMapping("/admin")
@@ -21,48 +14,18 @@ public class AdminController {
     private final AdminService adminService;
     private final RoleService roleService;
 
+    @Autowired
     public AdminController(AdminService adminService, RoleService roleService) {
         this.adminService = adminService;
         this.roleService = roleService;
     }
 
+    // Только показ главной страницы админки
     @GetMapping({"", "/"})
     public String showAdminPanel(Model model) {
         model.addAttribute("users", adminService.getAllUsers());
         model.addAttribute("currentUser", adminService.getCurrentUser());
-        model.addAttribute("user", new User());
         model.addAttribute("allRoles", roleService.getAllRoles());
         return "admin/admin";
-    }
-
-    @PostMapping("/new-user")
-    public String createUser(@ModelAttribute("user") User user,
-                             @RequestParam(value = "rolesSelected", required = false) List<Long> rolesSelected) {
-        adminService.createUser(user, rolesSelected);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/update/{id}")
-    public String editUserForm(@PathVariable("id") Long id, Model model) {
-        User user = adminService.getUserById(id);
-        if (user == null) {
-            return "redirect:/admin";
-        }
-        model.addAttribute("user", user);
-        model.addAttribute("allRoles", roleService.getAllRoles());
-        return "admin/update-user";
-    }
-
-    @PostMapping("/update")
-    public String updateUser(@ModelAttribute("user") User user,
-                             @RequestParam(value = "rolesSelected", required = false) List<Long> rolesSelected) {
-        adminService.updateUser(user, rolesSelected);
-        return "redirect:/admin";
-    }
-
-    @PostMapping("/delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        adminService.deleteUser(id);
-        return "redirect:/admin";
     }
 }
